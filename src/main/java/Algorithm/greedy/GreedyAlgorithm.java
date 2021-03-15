71613fc2e927a1736c646305f4e5c0b88aafeb66
hcy <ys-hucy@chinaunicom.cn>
2021-03-15 17:53:32 +0800

    greedy1

---
+++ src/main/java/Algorithm/greedy/GreedyAlgorithm.java
@@ -0,0 +1,13 @@
+package Algorithm.greedy;
+
+/**
+ * @author ：hcy
+ * @date ：Created in 2021/3/15
+ * @description ：
+ * @version: 1.0
+ */
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+        
+    }
+}
