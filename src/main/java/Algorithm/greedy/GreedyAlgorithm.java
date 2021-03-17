ab2b919a97f0bd841940b2e0b68fab4ed6f942ed
hcy <ys-hucy@chinaunicom.cn>
2021-03-17 16:48:22 +0800

    greedy2

--- src/main/java/Algorithm/greedy/GreedyAlgorithm.java
+++ src/main/java/Algorithm/greedy/GreedyAlgorithm.java
@@ -1,5 +1,9 @@
 package Algorithm.greedy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * @author ：hcy
  * @date ：Created in 2021/3/15
@@ -8,6 +12,49 @@ package Algorithm.greedy;
  */
 public class GreedyAlgorithm {
     public static void main(String[] args) {
-        
+        HashMap<String, HashSet<String>> broadcasts = new HashMap<>();
+        HashSet<String> hashSet1 = new HashSet<>();
+        hashSet1.add("北京");
+        hashSet1.add("上海");
+        hashSet1.add("天津");
+
+        HashSet<String> hashSet2 = new HashSet<>();
+        hashSet2.add("广州");
+        hashSet2.add("北京");
+        hashSet2.add("深圳");
+
+        HashSet<String> hashSet3 = new HashSet<>();
+        hashSet3.add("成都");
+        hashSet3.add("上海");
+        hashSet3.add("杭州");
+
+        HashSet<String> hashSet4 = new HashSet<>();
+
+        hashSet4.add("上海");
+        hashSet4.add("天津");
+
+        HashSet<String> hashSet5 = new HashSet<>();
+        hashSet5.add("杭州");
+        hashSet5.add("大连");
+
+        broadcasts.put("k1", hashSet1);
+        broadcasts.put("k2", hashSet2);
+        broadcasts.put("k3", hashSet3);
+        broadcasts.put("k4", hashSet4);
+        broadcasts.put("k5", hashSet5);
+
+        HashSet<String> allAreas = new HashSet<>();
+        allAreas.add("北京");
+        allAreas.add("上海");
+        allAreas.add("天津");
+        allAreas.add("广州");
+        allAreas.add("成都");
+        allAreas.add("杭州");
+        allAreas.add("大连");
+        allAreas.add("深圳");
+
+        ArrayList<String> selects = new ArrayList<>();
+
+
     }
 }
