36d9ee2e4a0e303dceaafafa72a4ce327be00a62
unknown <ys-hucy@chinaunicom.cn>
2021-04-14 16:20:55 +0800

    dijkstra1

---
+++ src/main/java/Algorithm/dijksrtra/DijkstraAlgorithm.java
@@ -0,0 +1,13 @@
+package Algorithm.dijksrtra;
+
+/**
+ * @author ：hcy
+ * @date ：Created in 2021/4/14
+ * @description ：
+ * @version: 1.0
+ */
+public class DijkstraAlgorithm {
+    public static void main(String[] args) {
+
+    }
+}
