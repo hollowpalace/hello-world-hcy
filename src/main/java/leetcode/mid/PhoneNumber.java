29190d166b546e2dfd1cb733bd04f89baa0672aa
hcyinhome <2818673282@qq.com>
2021-06-14 22:38:25 +0800

    1

---
+++ src/main/java/leetcode/mid/PhoneNumber.java
@@ -0,0 +1,16 @@
+package leetcode.mid;
+
+import java.util.List;
+
+public class PhoneNumber {
+    public static void main(String[] args) {
+
+    }
+
+    private static final char[][] c = new char[][]{{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'}, {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
+
+    public static List<String> letterCombinations(String digits) {
+        
+
+    }
+}
