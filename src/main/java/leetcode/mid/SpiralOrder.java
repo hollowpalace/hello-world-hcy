76eb5420358e95ecc203d1d70ae9b6d3c33f96d5
admin <admin>
2021-08-09 17:50:22 +0800

    1

---
+++ src/main/java/leetcode/mid/SpiralOrder.java
@@ -0,0 +1,24 @@
+package leetcode.mid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：hcy
+ * @date ：Created in 2021/8/9
+ * @description ：
+ * @version: 1.0
+ */
+public class SpiralOrder {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> res = new ArrayList<>();
+        int[][] ints = new int[m][n];
+
+
+    }
+}
